e59600a87fa76eb9d31d9f851bd8813b90173959
chava2708 <salvador95_2@hotmail.com>
2015-11-04 11:51:34 -0600

    Personaje
    Se agrego un personaje al proyecto

---
+++ Proyecto/Potopoly greenfoot/Jugador1.java
@@ -0,0 +1,19 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Jugador1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Jugador1 extends Actor
+{
+    /**
+     * Act - do whatever the Jugador1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
