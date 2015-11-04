51b3fe06afc2fdf9c29b88cd0a74d4db7fd5ac7a
chava2708 <salvador95_2@hotmail.com>
2015-11-04 11:46:17 -0600

    Actualizacion tab
    Se puso imagen de Tab

---
+++ Proyecto/Potopoly greenfoot/Tablero.java
@@ -0,0 +1,21 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class Tablero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tablero extends World
+{
+
+    /**
+     * Constructor for objects of class Tablero.
+     * 
+     */
+    public Tablero()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+    }
+}
