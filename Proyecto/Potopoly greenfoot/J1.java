aa7bbf8219f959f7c38856e735242dc4c0b24cc1
chava2708 <salvador95_2@hotmail.com>
2015-11-05 12:02:56 -0600

    Clases agregadas
    Carta
    Jugador
    Cromo
    Dinero
    Dado

--- Proyecto/Potopoly greenfoot/J1.java
+++ Proyecto/Potopoly greenfoot/J1.java
@@ -1,15 +1,15 @@
 import greenfoot.*;
 
 /**
- * Write a description of class Jugador1 here.
+ * Write a description of class J1 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Jugador1 extends Actor
+public class J1 extends Jugador
 {
     /**
-     * Act - do whatever the Jugador1 wants to do. This method is called whenever
+     * Act - do whatever the J1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
